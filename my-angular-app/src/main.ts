2436be87a3e13eaf5ad43a43bbdf1835969b8ed0
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-30 15:41:09 +0530

    ngTemplateOutlet

--- my-angular-app/src/main.ts
+++ my-angular-app/src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import {Component } from '@angular/core'
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,21 +7,42 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-  <h3>Structural directives micro-syntax</h3>
-  <button (click)="ok = !ok">Toggle</button>
-  <div *ngIf="ok; else other">OK</div>
-  <ng-template #other>Not OK</ng-template>
-  <ul>
-    <li *ngFor="let item of items; index as i">{{i}}. {{item}}</li>
-  </ul>
-  
+    <h3>Templates with ngTemplateOutlet</h3>
+
+    <label>
+      Type:
+      <select (change)="type = $any($event.target).value">
+        <option value="info">info</option>
+        <option value="warning">warning</option>
+        <option value="success">success</option>
+      </select>
+    </label>
+
+    <label>
+      Message: <input (input)="msg = $any($event.target).value" [value]="msg" />
+    </label>
+
+    <ng-container
+      [ngTemplateOutlet]="type === 'info' ? infoTpl : (type === 'warning' ? warnTpl : successTpl)"
+      [ngTemplateOutletContext]="{ $implicit: msg }">
+    </ng-container>
+
+    <ng-template #infoTpl let-text>
+      <p style="color:royalblue">Info: {{ text }}</p>
+    </ng-template>
+
+    <ng-template #warnTpl let-text>
+      <p style="color:darkorange">Warning: {{ text }}</p>
+    </ng-template>
+
+    <ng-template #successTpl let-text>
+      <p style="color:seagreen">Success: {{ text }}</p>
+    </ng-template>
   `
 })
 export class App {
-  ok = true
-    items = ['A','B','C'];
-
+  type = 'info';
+  msg = 'Hello';
 }
 
 bootstrapApplication(App);
-
